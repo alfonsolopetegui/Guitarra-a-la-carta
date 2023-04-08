8ce41b972b6d8f46b45177b5538c0babb3a1219c
Alfonso Lopetegui <alfonsolopetegui@hotmail.com>
2023-04-08 18:37:55 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const navbar = document.querySelector('.nav');
+
+window.onscroll = () => {
+    if(window.scrollY > 20) {
+        navbar.classList.add('nav-active')
+    } else {
+        navbar.classList.remove('nav-active')
+    }
+}
